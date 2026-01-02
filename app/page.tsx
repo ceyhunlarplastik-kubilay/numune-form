d68f5e462e4111411f3e7a37bdc95456bec1d518
Kubilay Uysal <kubilayuysal.ceyhunlarplastik@gmail.com>
2026-01-02 12:31:08 +0300

    Suspense added again

--- app/page.tsx
+++ app/page.tsx
@@ -5,9 +5,8 @@ import { useEffect, Suspense } from "react";
 import { useSearchParams, useRouter } from "next/navigation";
 import { toast } from "sonner";
 
-export default function Home() {
+function HomeContent() {
   const searchParams = useSearchParams();
-
   const router = useRouter();
 
   useEffect(() => {
@@ -22,28 +21,33 @@ export default function Home() {
   }, [searchParams, router]);
 
   return (
-    <Suspense fallback={<div className="p-6">Yükleniyor...</div>}>
-      <main
-        className="flex min-h-screen flex-col items-center justify-center 
-        bg-gradient-to-b from-[#ccb36e]/10 via-white to-white 
-        dark:from-[#ccb36e]/5 dark:via-zinc-900 dark:to-black
-        py-2"
-        // p-2 sm:p-4 md:p-8 lg:p-12
+    <main
+      className="flex min-h-screen flex-col items-center justify-center 
+      bg-gradient-to-b from-[#ccb36e]/10 via-white to-white 
+      dark:from-[#ccb36e]/5 dark:via-zinc-900 dark:to-black
+      py-2"
+    >
+      {/* Form Bileşeni */}
+      <div
+        className="
+        w-full 
+        max-w-7xl 
+        px-1                   /* ⬅ Mobilde minimum boşluk */
+        sm:px-3                /* ⬅ Tablet altı küçük ekran */
+        md:px-4                /* ⬅ Tablet */
+        lg:px-6                /* ⬅ Büyük ekran */
+      "
       >
-        {/* Form Bileşeni */}
-        <div
-          className="
-          w-full 
-          max-w-7xl 
-          px-1                   /* ⬅ Mobilde minimum boşluk */
-          sm:px-3                /* ⬅ Tablet altı küçük ekran */
-          md:px-4                /* ⬅ Tablet */
-          lg:px-6                /* ⬅ Büyük ekran */
-        "
-        >
-          <RequestForm3 />
-        </div>
-      </main>
+        <RequestForm3 />
+      </div>
+    </main>
+  );
+}
+
+export default function Home() {
+  return (
+    <Suspense fallback={<div className="p-6 text-center">Yükleniyor...</div>}>
+      <HomeContent />
     </Suspense>
   );
 }
