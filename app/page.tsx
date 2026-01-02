2e4c4f8c5a2df13ec532ad32b728b188df952fca
Kubilay Uysal <kubilayuysal.ceyhunlarplastik@gmail.com>
2026-01-02 12:13:08 +0300

    Suspense added for main page in order to use searchParams

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import RequestForm3 from "@/components/RequestForm3";
-import { useEffect } from "react";
+import { useEffect, Suspense } from "react";
 import { useSearchParams, useRouter } from "next/navigation";
 import { toast } from "sonner";
 
@@ -22,26 +22,28 @@ export default function Home() {
   }, [searchParams, router]);
 
   return (
-    <main
-      className="flex min-h-screen flex-col items-center justify-center 
-      bg-gradient-to-b from-[#ccb36e]/10 via-white to-white 
-      dark:from-[#ccb36e]/5 dark:via-zinc-900 dark:to-black
-      py-2"
-      // p-2 sm:p-4 md:p-8 lg:p-12
-    >
-      {/* Form Bileşeni */}
-      <div
-        className="
-        w-full 
-        max-w-7xl 
-        px-1                   /* ⬅ Mobilde minimum boşluk */
-        sm:px-3                /* ⬅ Tablet altı küçük ekran */
-        md:px-4                /* ⬅ Tablet */
-        lg:px-6                /* ⬅ Büyük ekran */
-      "
+    <Suspense fallback={<div className="p-6">Yükleniyor...</div>}>
+      <main
+        className="flex min-h-screen flex-col items-center justify-center 
+        bg-gradient-to-b from-[#ccb36e]/10 via-white to-white 
+        dark:from-[#ccb36e]/5 dark:via-zinc-900 dark:to-black
+        py-2"
+        // p-2 sm:p-4 md:p-8 lg:p-12
       >
-        <RequestForm3 />
-      </div>
-    </main>
+        {/* Form Bileşeni */}
+        <div
+          className="
+          w-full 
+          max-w-7xl 
+          px-1                   /* ⬅ Mobilde minimum boşluk */
+          sm:px-3                /* ⬅ Tablet altı küçük ekran */
+          md:px-4                /* ⬅ Tablet */
+          lg:px-6                /* ⬅ Büyük ekran */
+        "
+        >
+          <RequestForm3 />
+        </div>
+      </main>
+    </Suspense>
   );
 }
